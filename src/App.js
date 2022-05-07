b326205d7d5b63c72fd98855f1968a25ec93813d
Richa-rani02 <49235266+Richa-rani02@users.noreply.github.com>
2022-05-07 21:37:47 +0530

    feat:added home page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import {ToastWrapper} from "./utils/ToastWrapper";
 import {AllRoutes} from "./routes/AllRoutes";
+import "./App.scss";
 function App() {
   return (
     <div className="App">
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 import "./home.scss";
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className="home flex-center flex-col">
+    <div className='hero-img' >
+        <img className="responsive-img" src="../Assets/connectlogo.png"/>
+        </div>
+        <button className='home-btn py-0-75'>Login</button>
+
+
+    </div>
   )
 }
