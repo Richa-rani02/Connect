a27dd027f8cf26e9ae9a0b3d1014944cf315f734
Richa-rani02 <49235266+Richa-rani02@users.noreply.github.com>
2022-05-08 08:17:26 +0530

    fix:removed unused images

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="home flex-center flex-col">
     <div className='hero-img' >
-        <img className="responsive-img" src="../Assets/logog.png"/>
+        <img className="responsive-img" src="../Assets/connectlogo.png"/>
         </div>
         {/* <h1>Lets connect</h1> */}
         <button className='home-btn py-0-75'>Login</button>
