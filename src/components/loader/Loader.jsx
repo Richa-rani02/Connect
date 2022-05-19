e1b5f10c7ee24526f675458e973bed04adc39be7
Richa-rani02 <49235266+Richa-rani02@users.noreply.github.com>
2022-05-19 21:14:15 +0530

    feat:added loader

--- src/components/loader/Loader.jsx
+++ src/components/loader/Loader.jsx
@@ -2,7 +2,7 @@ import "./loader.scss";
 export const Loader=()=>{
     return(
         <div className="loader-wrapper flex-align-center">
-      <img src="" className="loader responsive-img"></img>
+      <img src="https://res.cloudinary.com/dgomw715r/image/upload/v1652974917/ProjectImages/blue-wheel-loading_cgrflm.gif" alt="loader" className="loader responsive-img"></img>
     </div>
     )
 }
