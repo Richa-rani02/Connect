78a37f1d3c4cd2d8789ad9f0745fb243222238ff
Richa-rani02 <49235266+Richa-rani02@users.noreply.github.com>
2022-05-20 10:47:25 +0530

    feat:follower UI

--- src/components/highlights/Highlights.jsx
+++ src/components/highlights/Highlights.jsx
@@ -1,16 +1,27 @@
 import "./highlights.scss";
 export const Highlights=()=>{
     return(
-        <div className="highlights p-1-5 flex flex-col flex-align-center">
-        <h3 className="highlights__title">
-            Top Highlights
+        <section className="user-suggested  flex flex-col flex-align-center p-0-5 ">
+        <h3 className="user-suggested__title mb-0-75">
+            Who to Follow
         </h3>
-        <div className="highlights__image my-1">
-        <img></img>
+        <div className="user_card flex flex-align-center px-1 mb-1">
+            <div className="flex user-details flex-align-center">
+            <span className="follow-img">
+            <img src="https://res.cloudinary.com/dgomw715r/image/upload/v1652983721/ProjectImages/IMG_20181016_135255_kccxuq.jpg" className="responsive-img"/>
+            </span>
+            <div>
+              <span>
+              <h4>name</h4>
+              </span>
+              <span>
+               <p>user</p>
+              </span>
+              </div>
+              </div>
+              <button className="follow-btn px-1 py-0-25">+ Follow</button>
         </div>
-        <li className="highlights__list">
-          <p></p>
-        </li>
-    </div>
+
+    </section>
     )
 }
