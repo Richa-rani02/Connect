7092503f0f3d48c7c222321611a7c7afe4d595ad
Richa-rani02 <49235266+Richa-rani02@users.noreply.github.com>
2022-06-05 00:57:26 +0530

    refactor:font

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,6 @@ export {InputBox} from "./inputBox/InputBox";
 export {Modal} from "./Modal/Modal";
 export {AvatarModal} from "./avatarModal/AvatarModal";
 export {Navbar} from "./navbar/Navbar";
-export {Footer} from "./footer/Footer";
 export {Sidebar} from "./sidebar/Sidebar";
 export {Highlights} from "./highlights/Highlights";
 export {Postcard} from "./postcard/Postcard";
