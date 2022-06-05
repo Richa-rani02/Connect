e4223e175bdf54f108a36a2a7c7dbbb8d0041906
Richa-rani02 <49235266+Richa-rani02@users.noreply.github.com>
2022-06-05 20:06:08 +0530

    feat:bookmark empty state

--- src/pages/Bookmark/Bookmark.jsx
+++ src/pages/Bookmark/Bookmark.jsx
@@ -1,17 +1,23 @@
 import { useSelector } from "react-redux";
 import { Navbar, Footer, Sidebar, Highlights, Postcard } from "../../components";
+import {Empty} from "../index";
+import { useLocation } from "react-router-dom";
 import { MainContainer } from "../mainContainer/MainContainer";
 import "./bookmark.scss";
 export const Bookmark = () => {
+   const location = useLocation();
    const { bookmark } = useSelector((state) => state.post);
    return (
       <div className="bookmark">
          <Navbar />
          <MainContainer leftchild={<Sidebar />} mainchild={
-            <section className="bookmark-section p-1 ">
-               {bookmark.map((posts) => (
+            <section className="bookmark-section p-1 ">{
+               bookmark.length>0 ?
+               bookmark.map((posts) => (
                   <Postcard key={posts._id} post={posts} />
-               ))}
+                  ))
+                :<Empty path={location.pathname}/>
+            }
             </section>
         } rightchild={<Highlights />} />
       </div>
--- src/pages/error/Empty.jsx
+++ src/pages/error/Empty.jsx
@@ -2,24 +2,25 @@ import "./Error.scss";
 import { useNavigate } from "react-router-dom";
 export const Empty=({path})=>{
     let message = "";
+    let image="";
     let navigate = useNavigate();
     switch (path) {
         case "/bookmark":
             {
-                img:"../Assets/bookmark.png";
+                image="../Assets/bookmark.png";
                 message = "You have not saved any post !!!"
             }
             break;
         case "/feed":
             {
-                img:"../Assets/no_result.png";
+                image="../Assets/no_result.png";
                 message = "Post something !!!"
             }
     }
     return(
         <div className="empty-page flex-center flex-col">
             <div className="empty-page__img">
-                <img src={img} className="responsive-img" alt="emptyimage"/>
+                <img src={image} className="responsive-img" alt="emptyimage"/>
             </div>
             <div className="empty-page__text">
                 <span >{message}</span>
