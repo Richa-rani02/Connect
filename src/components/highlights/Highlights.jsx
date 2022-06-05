21506647b66bd60ec7ae2e0698b12227228afdea
Richa-rani02 <49235266+Richa-rani02@users.noreply.github.com>
2022-06-05 15:27:12 +0530

    feat:added no suggestion user image

--- src/components/highlights/Highlights.jsx
+++ src/components/highlights/Highlights.jsx
@@ -1,34 +1,39 @@
-import { useSelector,useDispatch} from "react-redux";
+import { useSelector} from "react-redux";
 import "./highlights.scss";
-import { useEffect } from "react";
 import { BsSearch } from "../../utils/icons";
-import {followUnfollowUser} from "../../pages/profile/userSlice";
-import {FollowUserCard} from "./followUserCard/FollowUserCard";
-export const Highlights=()=>{
-  const dispatch=useDispatch();
-    const {allUsers}=useSelector((state)=>state.user);
-    const {userDetails}=useSelector((state)=>state.auth);
-    const suggestedUser=allUsers
-    .filter((user)=>user.username!=userDetails.username)
-    .filter((user)=>!userDetails.following.find((ele)=>ele._id===user._id))
-    return(
-        <section className="user-suggested  flex flex-col flex-align-center">
-          <article className="search-bar">
-          <form action="" className="search-form flex">
-             <span className="search-form__icon px-1 flex flex-align-center">
-             <BsSearch size={20} />
-             </span>
-        <input type="search" className="search-form__input" value=""placeholder="search user..."/>
-      </form>
-          </article>
-          <h3>
-           Suggestion for You
-          </h3>
-          <article className="user-list">
-          {suggestedUser?.map((user)=>(
-             <FollowUserCard userDetail={user}/>
-        ))}
-          </article> 
+import { FollowUserCard } from "./followUserCard/FollowUserCard";
+export const Highlights = () => {
+  const { allUsers } = useSelector((state) => state.user);
+  const { userDetails } = useSelector((state) => state.auth);
+  const suggestedUser = allUsers
+    .filter((user) => user.username != userDetails.username)
+    .filter((user) => !userDetails.following.find((ele) => ele._id === user._id))
+  return (
+    <section className="user-suggested  flex flex-col flex-align-center">
+      <article className="search-bar">
+        <form action="" className="search-form flex">
+          <span className="search-form__icon px-1 flex flex-align-center">
+            <BsSearch size={20} />
+          </span>
+          <input type="search" className="search-form__input" value="" placeholder="search user..." />
+        </form>
+      </article>
+      <h3>
+        Suggestion for You
+      </h3>
+      <article className="user-list">
+        {suggestedUser.length > 0 ?
+          suggestedUser?.map((user) => (
+            <FollowUserCard userDetail={user} />
+          )) :
+          <>
+          <div>
+            <img src="../Assets/no_result.png" className="responsive-img">
+            </img>
+          </div>
+          </>
+        }
+      </article>
     </section>
-    )
+  )
 }
--- src/components/highlights/followUserCard/FollowUserCard.jsx
+++ src/components/highlights/followUserCard/FollowUserCard.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import {useDispatch} from "react-redux";
 import "./followUserCard.scss";
 import {Avatar} from "../../index";
+import {followUnfollowUser} from "../../../pages/profile/userSlice";
 export const FollowUserCard = ({userDetail}) => {
+  const dispatch=useDispatch();
   return (
     <div className="user_card flex flex-align-center  mb-0-5 px-0-5" key={userDetail.id}>
             <div className="flex user-details flex-align-center">
@@ -15,7 +18,7 @@ export const FollowUserCard = ({userDetail}) => {
               </span>
               </div>
               </div>
-              <button className="follow-btn px-1 py-0-25"> Follow</button>
+              <button className="follow-btn px-1 py-0-25"  onClick={()=>dispatch(followUnfollowUser({userId:userDetail._id,dispatch:dispatch,isFollowing:false}))}>Follow</button>
         </div>
   )
 }
