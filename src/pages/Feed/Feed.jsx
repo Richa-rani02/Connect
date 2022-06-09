87ae07ae1f80b7eed2e473f2af1cba35a5c06365
Richa-rani02 <49235266+Richa-rani02@users.noreply.github.com>
2022-06-09 15:02:28 +0530

    refactor:add post UI fix

--- src/pages/Feed/Feed.jsx
+++ src/pages/Feed/Feed.jsx
@@ -1,10 +1,11 @@
-import { Navbar, Sidebar, Highlights, Postcard, Loader, EmojisPicker, Avatar, SideContainer, BirthdayCard } from "../../components";
+import { Navbar, Sidebar, Highlights, Postcard, Loader, EmojisPicker, SideContainer, BirthdayCard } from "../../components";
 import "./feed.scss";
 import { Empty } from "../index";
 import { useLocation } from "react-router-dom";
 import { getAllPost, addPost } from "../../redux/postSlice";
 import { useEffect, useState, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import Avatar from "@mui/material/Avatar";
 import { FcRightUp, FcGenericSortingAsc, FcGenericSortingDesc } from "../../utils/icons";
 import { MainContainer } from "../mainContainer/MainContainer";
 import { CreatePost } from "./createPost/CreatePost";
@@ -13,14 +14,16 @@ export const Feed = () => {
     const location = useLocation();
     const { allPosts, isLoading, postStatus } = useSelector((state) => state.post);
     const { userDetails } = useSelector((state) => state.auth);
+    const { user} = useSelector((state) => state.auth);
     const [feedPost, setFeedPost] = useState([]);
     const [trendingPost, setTrendingPost] = useState({ isTrending: false, posts: [] });
     const [filterText, setFilterText] = useState("");
-    const [postContent, setPostContent] = useState(
-        {
-            content: "",
-            pic: "",
-        });
+    const [createPostActive , setCreatePostActive]=useState(true);
+    // const [postContent, setPostContent] = useState(
+    //     {
+    //         content: "",
+    //         pic: "",
+    //     });
 
     useEffect(() => {
         if (postStatus == 'idle') {
@@ -36,12 +39,13 @@ export const Feed = () => {
             )
         }
     }, [userDetails, allPosts])
+    const handlecreatePostToogle = () => setCreatePostActive((prev) => !prev);
 
-    const postHandler = (e) => {
-        e.preventDefault();
-        dispatch(addPost({ ...postContent }));
-        setPostContent({ content: "", pic: "" });
-    }
+    // const postHandler = (e) => {
+    //     e.preventDefault();
+    //     dispatch(addPost({ ...postContent }));
+    //     setPostContent({ content: "", pic: "" });
+    // }
 
     const trendHandler = () => {
         setTrendingPost((prev) => ({
@@ -60,45 +64,54 @@ export const Feed = () => {
         setFeedPost(feedPost?.sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt)));
     }
     return (
+        <>
         <div className="feed">
             <Navbar />
             <MainContainer leftchild={
 
                 <Sidebar />
             } mainchild={
-                // <section className="main-section flex flex-col">
-                //     <article className="">
-                //         <CreatePost userDetails={userDetails} postContent={postContent} setPostContent={setPostContent} postHandler={postHandler} />
+                <section className="main-section flex flex-col">
+                    <article className="">
+                        <div className="input-click-container flex flex-align-center p-0-25">
+                        <Avatar sx={{ height: '52px', width: '52px', backgroundColor:'#818cf8'}} src={user?.profileImg || user?.firstName?.charAt(0)} alt={user?.firstName} />
+                         <div className="input-click p-0-5" onClick={()=>setCreatePostActive(true)}>
+                         What's on your mind ?
+                         </div>
+                        </div>
+                        {/* <CreatePost userDetails={userDetails} postContent={postContent} setPostContent={setPostContent} postHandler={postHandler} /> */}
 
-                //         <div className="post-header flex py-0-5">
-                //             <h4 onClick={trendHandler}><span><FcRightUp size={20} /></span>Trending</h4>
-                //             <h4 onClick={oldestHandler}><span><FcGenericSortingAsc size={20} /></span>Oldest</h4>
-                //             <h4 onClick={latestHandler}><span><FcGenericSortingDesc size={20} /></span>Latest</h4>
-                //         </div>
-                //     </article>
-                //     <article className="post-list">
+                        {/* <div className="post-header flex py-0-5">
+                            <h4 onClick={trendHandler}><span><FcRightUp size={20} /></span>Trending</h4>
+                            <h4 onClick={oldestHandler}><span><FcGenericSortingAsc size={20} /></span>Oldest</h4>
+                            <h4 onClick={latestHandler}><span><FcGenericSortingDesc size={20} /></span>Latest</h4>
+                        </div> */}
+                    </article>
+                    {/* <article className="post-list">
 
-                //         {isLoading ?
-                //             <Loader /> : trendingPost.isTrending ? (
-                //                 <>
-                //                     {trendingPost.posts.length > 0 ? (
-                //                         [...trendingPost.posts].map((posts) => <Postcard key={posts.id} post={posts} />)
-                //                     ) : <Empty path="/liked" />}
-                //                 </>
-                //             ) :
-                //                 <>
-                //                     {feedPost.length !== 0 ? (
-                //                         feedPost?.map((posts) => (
-                //                             <Postcard key={posts.id} post={posts} setPostContent={setPostContent} postContent={postContent} />
-                //                         ))
-                //                     ) : <Empty path={location.pathname} />}
+                        {isLoading ?
+                            <Loader /> : trendingPost.isTrending ? (
+                                <>
+                                    {trendingPost.posts.length > 0 ? (
+                                        [...trendingPost.posts].map((posts) => <Postcard key={posts.id} post={posts} />)
+                                    ) : <Empty path="/liked" />}
+                                </>
+                            ) :
+                                <>
+                                    {feedPost.length !== 0 ? (
+                                        feedPost?.map((posts) => (
+                                            <Postcard key={posts.id} post={posts} setPostContent={setPostContent} postContent={postContent} />
+                                        ))
+                                    ) : <Empty path={location.pathname} />}
 
-                //                 </>}
-                //     </article>
-                // </section>
+                                </>}
+                    </article> */}
+                </section>
+// {/* <></> */}
 
-<></>
             } rightchild={<Highlights />} />
         </div>
+        <CreatePost isOpen={createPostActive} onClose={handlecreatePostToogle}/>
+        </>
     )
 }
--- src/pages/Feed/createPost/CreatePost.jsx
+++ src/pages/Feed/createPost/CreatePost.jsx
@@ -1,56 +1,102 @@
 import React from 'react'
-import { EmojisPicker, Avatar } from "../../../components/index";
+import { EmojisPicker, Modal } from "../../../components/index";
 import { FcPicture } from "../../../utils/icons";
-import { useState,useRef } from 'react';
+import Avatar from "@mui/material/Avatar";
+import { useState, useRef } from 'react';
+import { useSelector } from 'react-redux';
 import "./createPost.scss";
-export const CreatePost = ({ userDetails, postContent, setPostContent, postHandler }) => {
+export const CreatePost = ({isOpen, onClose }) => {
+    const { user} = useSelector((state) => state.auth);
     const [emojiPickerActive, setEmojiPickerActive] = useState(false);
     const fileInput = useRef(null);
- 
- const imageUpload = async (e) => {
-    const file = e.target.files[0];
-    const toBase64 = (file) =>
-      new Promise((resolve, reject) => {
-        const reader = new FileReader();
-        reader.readAsDataURL(file);
-        reader.onload = () => resolve(reader.result);
-        reader.onerror = (error) => reject(error);
-      });
+    
+    const [postContent, setPostContent] = useState(
+        {
+            content: "",
+            pic: "",
+        });
 
-    let base64File = await toBase64(file);
-    setPostContent({ ...postContent, pic: base64File });
-  };
+    const postHandler = (e) => {
+        e.preventDefault();
+        // dispatch(addPost({ ...postContent }));
+        setPostContent({ content: "", pic: "" });
+    }    
+
+    const imageUpload = async (e) => {
+        const file = e.target.files[0];
+        const toBase64 = (file) =>
+            new Promise((resolve, reject) => {
+                const reader = new FileReader();
+                reader.readAsDataURL(file);
+                reader.onload = () => resolve(reader.result);
+                reader.onerror = (error) => reject(error);
+            });
+
+        let base64File = await toBase64(file);
+        setPostContent({ ...postContent, pic: base64File });
+    };
     return (
-        <form className="create-form flex-col px-1 py-0-25">
-            <div className="flex create-form__container">
-                <Avatar details={userDetails} className="md" />
-                <textarea
-                    value={postContent.content}
-                    type="text"
-                    placeholder="What's on your mind ?"
-                    name=""
-                    rows="3"
-                    onChange={(e) => setPostContent({ ...postContent, content: e.target.value })}
-                />
-            </div>
-            {postContent.pic &&
-                <div className='flex flex-center'>
-                    <div className='post-img'>
-                        <img src={postContent.pic} className='responsive-img'></img>
-                    </div>
-                </div>}
-
-            <div className="footer-icons py-0-75 flex">
-                <div className="footer-icons__left flex px-1">
-
-                    <span onClick={() => fileInput.current.click()}><FcPicture size={24} /><input ref={fileInput} type="file" style={{display:'none'}} onChange={imageUpload}/></span>
-                    <span onClick={() => setEmojiPickerActive((prev) => !prev)}><span className="emoji">🙂</span></span>
-                    <EmojisPicker emojiActive={emojiPickerActive} setPostContent={setPostContent} postContent={postContent} />
+        <Modal isOpen={isOpen} onClose={onClose}>
+            <form className='flex flex-col create-form'>
+                <div className='form-header flex'>
+                    <Avatar sx={{ height: '52px', width: '52px', backgroundColor: '#818cf8' }} src={user?.profileImg || user?.firstName?.charAt(0)} alt={user?.firstName} />
+                    <textarea
+                        value={postContent.content}
+                        type="text"
+                        name=""
+                        rows={2}
+                     onChange={(e) => setPostContent({ ...postContent, content: e.target.value })}
+                    />
                 </div>
-                <div className="footer-icons__right">
-                    <button className="post-btn px-1-5 py-0-5" onClick={postHandler}>Post</button>
+                <div className='form-image'>
+                    <img src='../Assets/no_result.png' className='responsive-img'>
+                    </img>
+
                 </div>
-            </div>
-        </form>
+                <div className='form-footer flex flex-align-center'>
+                  <div className="footer-icons__left flex px-1">
+                  {/* onClick={() => fileInput.current.click()} */}
+                  <span onClick={() => fileInput.current.click()}><FcPicture size={24} /><input ref={fileInput} type="file" style={{display:'none'}} onChange={imageUpload}/></span>
+                  <span onClick={() => setEmojiPickerActive((prev) => !prev)}><span className="emoji">🙂</span></span>
+                  <EmojisPicker emojiActive={emojiPickerActive} setPostContent={setPostContent} postContent={postContent} />
+                  </div>
+                  <button className="post-btn px-1-5 py-0-5" onClick={postHandler}>Post</button>
+                </div>
+
+            </form>
+
+        </Modal>
+
+        // <form className="create-form flex-col px-1 py-0-25">
+        //     <div className="flex create-form__container">
+        //         <Avatar details={userDetails} className="md" />
+        //         <textarea
+        //             value={postContent.content}
+        //             type="text"
+        //             placeholder="What's on your mind ?"
+        //             name=""
+        //             rows="3"
+        //             onChange={(e) => setPostContent({ ...postContent, content: e.target.value })}
+        //         />
+        //     </div>
+        //     {postContent.pic &&
+        //         <div className='flex flex-center'>
+        //             <div className='post-img'>
+        //                 <img src={postContent.pic} className='responsive-img'></img>
+        //             </div>
+        //         </div>}
+
+        //     <div className="footer-icons py-0-75 flex">
+        //         <div className="footer-icons__left flex px-1">
+
+        //             <span onClick={() => fileInput.current.click()}><FcPicture size={24} /><input ref={fileInput} type="file" style={{display:'none'}} onChange={imageUpload}/></span>
+        //             <span onClick={() => setEmojiPickerActive((prev) => !prev)}><span className="emoji">🙂</span></span>
+        //             <EmojisPicker emojiActive={emojiPickerActive} setPostContent={setPostContent} postContent={postContent} />
+        //         </div>
+        //         <div className="footer-icons__right">
+        //             <button className="post-btn px-1-5 py-0-5" onClick={postHandler}>Post</button>
+        //         </div>
+        //     </div>
+        // </form>
     )
 }
